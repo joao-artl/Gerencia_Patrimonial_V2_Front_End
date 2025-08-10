83a0e6ebc296938a74e41d8592d88a76233c3b6c
joao-artl <jalxpinheiro@gmail.com>
2025-08-09 22:16:48 -0300

    feat(refresh token): altera arquivo da api para permitir a utilização do refresh token

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
 });
 
 api.interceptors.request.use(
@@ -17,8 +17,53 @@ api.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const refreshToken = localStorage.getItem('refreshToken');
+        
+        if (!refreshToken) {
+          console.error("Refresh token não encontrado. Deslogando...");
+          localStorage.clear();
+          window.location.href = '/';
+          return Promise.reject(error);
+        }
+        const response = await axios.post(
+          `${process.env.NEXT_PUBLIC_API_URL}/api/token/refresh/`,
+          {
+            refresh: refreshToken,
+          }
+        );
+
+        const { access, refresh } = response.data;
+        localStorage.setItem('accessToken', access);
+        if (refresh) {
+          localStorage.setItem('refreshToken', refresh);
+        }
+        originalRequest.headers.Authorization = `Bearer ${access}`;
+        return api(originalRequest);
+
+      } catch (refreshError) {
+        console.error("Não foi possível renovar a sessão. Por favor, faça login novamente.", refreshError);
+        localStorage.clear();
+        window.location.href = '/';
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 export const apiSemToken = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`,
   headers: {
     'Content-Type': 'application/json',
   },
